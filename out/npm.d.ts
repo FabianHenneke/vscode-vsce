af58d16e61687373c2adcce3a04492245c45381f
Fabian Meumertzheim <meumertzheim@code-intelligence.com>
2020-11-20 08:09:44 +0100

    Publish

---
+++ out/npm.d.ts
@@ -0,0 +1,9 @@
+import { CancellationToken } from './util';
+export interface YarnDependency {
+    name: string;
+    path: string;
+    children: YarnDependency[];
+}
+export declare function detectYarn(cwd: string): Promise<boolean>;
+export declare function getDependencies(cwd: string, useYarn?: boolean, packagedDependencies?: string[]): Promise<string[]>;
+export declare function getLatestVersion(name: string, cancellationToken?: CancellationToken): Promise<string>;
